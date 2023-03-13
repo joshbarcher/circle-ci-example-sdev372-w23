b344ad6b1289e2b267238de0f75389542a330eaa
Josh Archer <jarcher@greenriver.edu>
2023-03-13 14:22:57 -0700

    Removed exceptions from the MyMath class.

--- src/main/java/org/example/math/MyMath.java
+++ src/main/java/org/example/math/MyMath.java
@@ -4,10 +4,10 @@ public class MyMath
 {
     public double divide(double num, double den)
     {
-        if (den == 0)
+        /*if (den == 0)
         {
             throw new ArithmeticException("Cannot divide by zero");
-        }
+        }*/
         return num / den;
     }
 
